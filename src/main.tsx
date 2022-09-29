ac7b8713b1baff9356c1d5a8342c818712ec5011
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2022-09-29 17:02:59 -0300

    install recoiljs and recoiljs-persist

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RecoilRoot } from 'recoil'
 import { AppRoutes } from './routes'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AppRoutes />
+    <RecoilRoot>
+      <AppRoutes />
+    </RecoilRoot>
   </React.StrictMode>
 )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,10 @@
 import reactLogo from '../assets/react.svg'
+import { useRecoilState } from 'recoil'
+import { counterState } from '../store'
 
 export function Home() {
+  const [count, setCount] = useRecoilState(counterState)
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
@@ -13,6 +17,11 @@ export function Home() {
         </a>
       </div>
       <h1>Vite + React + TS + TailwindCSS</h1>
+      <div>
+        <h3>Counter: {count}</h3>
+        <button onClick={() => setCount(count + 1)}>Increase</button>
+        <button onClick={() => setCount(count - 1)}>Decrease</button>
+      </div>
     </div>
   )
 }
